b26856dceeb2e24ee4b194d94c76961871266a9c
francid <francid81@gmail.com>
2016-01-26 16:33:52 -0500

    Added Cloud Object and its script CloudMover

---
+++ Assets/_Scripts/CloudMover.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CloudMover : MonoBehaviour {
+
+	// Public Variables
+	public float speed;
+
+	//Private Variables
+	private Transform transform;
+	private Vector2 cloudPosition;
+
+	// Use this for initialization
+	void Start () {
+		this.transform = gameObject.GetComponent<Transform> ();
+		this.Reset ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.cloudPosition = this.transform.position;
+		this.cloudPosition -= new Vector2 (this.speed, 0.0f);
+		this.transform.position = this.cloudPosition;
+
+		if (this.cloudPosition.x <= -608) {
+			this.Reset ();
+		}
+	}
+
+	public void Reset(){
+		this.transform.position = new Vector2 (608f, 0.0f);
+	}
+}
