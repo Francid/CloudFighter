c11d50d006c1679a1ef6dab6b7af20fdf5e73cfb
francid <francid81@gmail.com>
2016-01-26 22:43:38 -0500

    Added Fighter script called FighterController

---
+++ Assets/_Scripts/FighterController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FighterController : MonoBehaviour {
+
+	// PUBLIC VARIABLES
+	public float speed;
+
+	// PRIVATE VARIABLES
+	private Transform transform;
+	private float moveVertical;
+	private Vector2 currentPostion;
+
+	// Use this for initialization
+	void Start () {
+		transform = gameObject.GetComponent<Transform> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.currentPostion = this.transform.position;
+
+		this.moveVertical = Input.GetAxis ("Vertical");
+		if (this.moveVertical < 0) {
+			this.currentPostion -= new Vector2 (0.0f, this.speed);
+		}
+		if (this.moveVertical > 0) {
+			this.currentPostion += new Vector2 (0.0f, this.speed);
+		}
+		this.transform.position = this.currentPostion;
+	}
+}
